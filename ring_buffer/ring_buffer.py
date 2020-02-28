3044da6d854bfbd59a9b2a8802d98e9fafc70825
BrandonLively <akutenshi@live.com>
2020-02-28 12:06:41 -0500

    Ring Buffer MVP Met #Modified DLL add to head and add to tail to return head and tail Could have been handled by either making a new class that inheriates from it, or having a two step operation in the ring_buffer code.

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -8,12 +8,24 @@ class RingBuffer:
         self.storage = DoublyLinkedList()
 
     def append(self, item):
-        pass
+        if len(self.storage) < self.capacity:
+            self.current = self.storage.add_to_tail(item)
+        else:
+            if self.current == self.storage.tail:
+                self.storage.head.value = item
+                self.current = self.storage.head
+            else:
+                self.current.next.value = item
+                self.current = self.current.next
+
 
     def get(self):
         # Note:  This is the only [] allowed
         list_buffer_contents = []
-
+        current_node = self.storage.head
+        for i in range(len(self.storage)):
+            list_buffer_contents.append(current_node.value)
+            current_node = current_node.next
         # TODO: Your code here
 
         return list_buffer_contents
